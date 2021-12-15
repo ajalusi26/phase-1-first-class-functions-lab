cef3ddd0cd8c7bdd8cd1e0245d950cec49447241
AJ Jalusi <ajalusi26@gmail.com>
2021-12-14 19:41:44 -0500

    commit tree 2c28d3c24bf2a3f90b0be8b9aa460d8df33cdf33

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers)  => { 
+    let newDrivers = drivers.slice(0,2)
+    return newDrivers;
+
+}
+const returnLastTwoDrivers = (drivers) => {
+    let newDrivers = drivers.slice(-2)
+    return newDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(){
+    return function multiply(x){
+        return x * x;
+    }
+}
+
+function fareDoubler(x) {
+    return x*2
+}
+
+function fareTripler(x){
+    return x*3
+}
+function selectDifferentDrivers(drivers, func){
+    return func(drivers)
+}
